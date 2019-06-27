72579f20e6bc592a2158e7898bddca4d11ad2b1f
User <User@192.168.1.17>
2019-06-28 02:15:11 +0700

    small update homepage and login

--- src/test/java/Method/Common/Login.java
+++ src/test/java/Method/Common/Login.java
@@ -12,47 +12,19 @@ public class Login extends BasedActions {
 	}
 	
 	public void inputUID (String input, ExtentTest test) {
-		inputText(driverLogIn, Abs.Common.Login.baseUrl, input, test);;
+		inputText(driverLogIn, Abs.Common.Login.userID, input, test);;
 	}
 	
 	public void inputPass (String input, ExtentTest test) {
 		inputText(driverLogIn, Abs.Common.Login.pass, input, test);;
 	}
 	
-	public HomepageAction clickLogin (ExtentTest test) {
-		clickBtn(driverlocal, LoginInterFace.loginBtn, test);
-		return ManageDriver.manageHomePage(driverlocal);
-	}
-	
-	public InputEmailActions clickHere (ExtentTest test) {
-		clickBtn(driverlocal, LoginInterFace.linkHere, test);
-		return ManageDriver.manageInputEmailPage(driverlocal);
-	}
-
-	public void cancelAlert() {
-		closeAlert(driverlocal);
-	}
-	
-	public void assertText() {
-		String abc = alertText(driverlocal);
-		Boolean result = abc.equals("User or Password is not valid");
-		if (result == true) {
-			System.out.println("Alert match");
-		} else {
-			System.out.println("Alert mismatch");
-		}
-	}
-	
-	public void verifyEle() {
-		if (isAlertPresent(driverlocal) == true) {
-			System.out.println("User or Password is not valid");
-		}else {
-			System.out.println("Error, Alert doesn't show");
-		}
+	public void clickLogin (ExtentTest test) {
+		clickBtn(driverLogIn, Abs.Common.Login.loginBtn, test);
 	}
 
 	public void openPage(ExtentTest test) {
-		OpenURL(driverlocal, LoginInterFace.baseUrl, test);
+		OpenURL(driverLogIn, Abs.Common.Login.baseUrl, test);
 	}
 	
 	
---
+++ src/test/java/Method/Common/homePage.java
@@ -0,0 +1,10 @@
+package Method.Common;
+
+import org.openqa.selenium.WebDriver;
+
+public class homePage {
+	WebDriver driverHomePage;
+	public homePage(WebDriver driver) {
+		driverHomePage = driver;
+	}
+}
